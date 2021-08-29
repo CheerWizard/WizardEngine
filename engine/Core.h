6a41b86964eb582d863bb4901771a7e82795535e
MercuriiWorks <mechanik2442@gmail.com>
2021-08-29 11:15:16 +0300

    Fixing CMake build and linking bugs.

--- engine/Core.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by mecha on 28.08.2021.
-//
-/**
- * Provides all sources of engine lib.
- * */
-#include "Application.h"
---
+++ engine/Engine.h
@@ -0,0 +1,7 @@
+//
+// Created by mecha on 28.08.2021.
+//
+/**
+ * A library sources provider.
+ * */
+#include "core/Application.h"
