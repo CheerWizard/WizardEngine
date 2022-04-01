093d2462ea3cbc07e1665a98aaf7e23ddf3233ce
MercuriiWorks <mechanik2442@gmail.com>
2022-04-01 17:08:53 +0300

    Finally, off-screen MSAA done!

---
+++ WizardEngine/src/cpp/graphics/core/geometry/Screen.cpp
@@ -0,0 +1,20 @@
+//
+// Created by mecha on 01.04.2022.
+//
+
+#include <graphics/core/geometry/Screen.h>
+
+namespace engine {
+
+    Screen::Screen() {
+        auto vertices = new ScreenVertex[4] {
+                ScreenVertex { { -0.5, -0.5 }, { 0, 0 } },
+                ScreenVertex { { 0.5, -0.5 }, { 1, 0 } },
+                ScreenVertex { { 0.5, 0.5 }, { 1, 1 } },
+                ScreenVertex { { -0.5, 0.5 }, { 0, 1 } }
+        };
+        vertexData = VertexData<ScreenVertex> { vertices, 0, 4 };
+        drawType = QUAD;
+    }
+
+}
